5ffeabb2e6193dd25c99f6aabde1b2633b145d7c
Ray Soyama <raysoyama@gmail.com>
2020-06-28 14:14:59 -0700

    CycleOffset

---
+++ Assets/_eJam2020/Scripts/CycleOffset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleOffset : MonoBehaviour
+{
+    public List<Animator> allCharacters = new List<Animator>();
+    void Start()
+    {
+        foreach (Animator anim in allCharacters)
+        {
+            anim.SetFloat("Offset", Random.Range(0.0f, anim.GetCurrentAnimatorStateInfo(0).length));
+        }
+    }
+}
